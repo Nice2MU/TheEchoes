8dbfc31da2a36b6fa26589a35cb990444b37b75c
Nice2MU <nice2mu.hub@gmail.com>
2025-03-04 12:55:29 +0700

    First Commit
    First Commit for TheEchoes Project

---
+++ TheEchoes/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpForce = 7f;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    private Animator animator;
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    private bool isGrounded;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveX * speed, rb.velocity.y);
+
+        animator.SetBool("walk", moveX != 0);
+        if (moveX != 0) spriteRenderer.flipX = moveX < 0;
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+        animator.SetBool("isGrounded", isGrounded);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            animator.SetTrigger("jump");
+        }
+    }
+}
---
+++ TheEchoes/Assets/Scripts/SceneManagers.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Concurrent;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagers : MonoBehaviour
+{
+    public void LoadScene(string scenename)
+    {
+        SceneManager.LoadScene(scenename);
+    }
+}
